690ab5fbc53699dd43a7d64e456f70d37be6f118
Mills-44 <mylesbrodsky44@gmail.com>
2025-06-06 18:48:23 -0500

    Update utility.lua

--- lib/utility.lua
+++ lib/utility.lua
@@ -1,5 +1,5 @@
 -- Load Files for anything for mod
-function MILLS.register_items(items, path)
+function FMAN.register_items(items, path)
   for i = 1, #items do
     SMODS.load_file(path .. "/" .. items[i] .. ".lua")()
   end
