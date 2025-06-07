68f537710f6cbda078af28975ae7c74d59f3a42d
Mills-44 <mylesbrodsky44@gmail.com>
2025-06-06 21:56:20 -0500

    Delete lib directory

--- lib/define.lua
+++
@@ -1,6 +0,0 @@
-FMAN.config = SMODS.current_mod.config
-
--- Loads Files For Jokers
-FMAN.JOKERS = {
-    --"" ,
-}
--- lib/utility.lua
+++
@@ -1,6 +0,0 @@
--- Load Files for anything for mod
-function FMAN.register_items(items, path)
-  for i = 1, #items do
-    SMODS.load_file(path .. "/" .. items[i] .. ".lua")()
-  end
-end
