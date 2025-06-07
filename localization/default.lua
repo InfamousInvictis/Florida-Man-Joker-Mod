57078fc647ef4ae507568420418f7f0e18de707b
Mills-44 <mylesbrodsky44@gmail.com>
2025-06-06 21:55:33 -0500

    Add files via upload

---
+++ localization/default.lua
@@ -0,0 +1,23 @@
+return {
+	descriptions = {
+		Joker = {
+			j_flor_jawsome = {
+				name = 'Jaw-some',
+				text = {
+					"When a {C:attention}Blind is {C:attention}skipped{}, an {C:dark_edition}Edition{}",
+					"is applied to a {C:attention}random Joker{}",
+				}
+			},
+			j_flor_liftoff = {
+				name = 'Liftoff!',
+				text = {
+					"Scoring {C:attention}A to 5 Straight's{}",
+					"Increases money by {C:gold}$1{}",
+					"when played hands score",
+					"{C:inactive}Total:{}{C:gold} $#1#",
+					"{C:inactive}Resets Every Boss Blind"
+				}
+			},
+		}
+	}
+}
