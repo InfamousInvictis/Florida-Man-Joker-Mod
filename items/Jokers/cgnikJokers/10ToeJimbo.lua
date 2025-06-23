078663bb6efbfbc22aa506dfa187df1e4e4f3377
RattlingSnow353 <84494581+RattlingSnow353@users.noreply.github.com>
2025-06-23 19:50:46 -0400

    Improved loading system + fixed sprite issues

---
+++ items/Jokers/cgnikJokers/10ToeJimbo.lua
@@ -0,0 +1,47 @@
+local ToeJimbo_atlas = {
+	object_type = "Atlas", 
+	key = "FloridaJawsome", 
+	path = "jawsome.png", 
+	px = 71, 
+	py = 95
+}
+
+local ToeJimbo = {
+    object_type = "Joker",
+    order = 1,
+    key = "10ToeJimbo",
+    config = { 
+        extra = {
+            dollars = 10
+        }
+    }
+    rarity = 2,
+    --atlas = 'FloridaJawsome',
+	pos = { x = 0, y = 0 },
+    cost = 7,
+    unlocked = true,
+    discovered = false,
+    blueprint_compat = true,
+    eternal_compat = true,
+    perishable_compat = true,
+  
+    loc_vars = function(self,info_queue,card)
+        return {vars = {
+            card.ability.extra.dollars,
+            colours={G.C.DARK_EDITION}
+        }}
+    end,
+
+    calculate = function(self,card,context)
+        if context.after and context.main_eval then
+            local chipsThisHand = hand_chips * mult
+            local requirement = G.GAME.blind.chips
+            if chipsThisHand > requirement then
+                return {
+                    dollars = 10
+                }
+            end
+        end
+    end
+}
+return { name = {"Jokers"}, items = { ToeJimbo} }
